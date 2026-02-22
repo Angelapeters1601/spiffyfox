9ca14158125848f42cc06ba7939a6a8137732c68
TOOMAMA <nwattaha@gmail.com>
2026-02-22 18:59:33 +0100

    refine admin faq page ui and update footer link

--- src/admin/tips/AdminTips.jsx
+++ src/admin/tips/AdminTips.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { supabase } from "../../services/supabaseClient";
 import { getYouTubeVideoDetails } from "../../services/youtubeAPI";
 import {
